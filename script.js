d3f489e16781ffb9819c777f34cf083425b8ceaa
Caio Pereira Mascarenhas <caio.pereira1998@hotmail.com>
2025-12-30 10:08:36 -0300

    feat: new layout

--- script.js
+++ script.js
@@ -116,6 +116,118 @@ const jogosEinstein = [
   { id: 24, name: "Jogo 24", numbers: [13, 22, 45, 46, 59, 60] },
 ];
 
+const jogosNewSet = [
+  // ================= JESSICA =================
+  { id: 1, name: "Jessica", numbers: [7, 16, 26, 35, 44, 51] },
+  { id: 2, name: "Jessica", numbers: [8, 10, 13, 28, 44, 54] },
+  { id: 3, name: "Jessica", numbers: [2, 4, 22, 24, 46, 47] },
+  { id: 4, name: "Jessica", numbers: [12, 14, 29, 38, 49, 53] },
+  { id: 5, name: "Jessica", numbers: [18, 19, 40, 42, 57, 59] },
+  { id: 6, name: "Jessica", numbers: [3, 10, 35, 36, 37, 41] },
+  { id: 7, name: "Jessica", numbers: [1, 5, 17, 50, 51, 56] },
+  { id: 8, name: "Jessica", numbers: [20, 32, 33, 34, 58, 60] },
+  { id: 9, name: "Jessica", numbers: [6, 12, 24, 36, 48, 60] },
+  { id: 10, name: "Jessica", numbers: [2, 8, 18, 28, 36, 58] },
+  { id: 11, name: "Jessica", numbers: [9, 14, 27, 35, 39, 57] },
+  { id: 12, name: "Jessica", numbers: [1, 5, 14, 23, 33, 38] },
+  { id: 13, name: "Jessica", numbers: [13, 22, 26, 40, 45, 57] },
+  { id: 14, name: "Jessica", numbers: [7, 18, 25, 31, 42, 56] },
+  { id: 15, name: "Jessica", numbers: [9, 16, 27, 33, 41, 58] },
+  { id: 16, name: "Jessica", numbers: [13, 21, 34, 47, 52, 59] },
+  { id: 17, name: "Jessica", numbers: [28, 30, 37, 46, 49, 55] },
+  { id: 18, name: "Jessica", numbers: [19, 22, 29, 35, 43, 57] },
+  { id: 19, name: "Jessica", numbers: [20, 32, 38, 44, 45, 53] },
+  { id: 20, name: "Jessica", numbers: [14, 23, 24, 40, 50, 60] },
+
+  // ================= ATILA =================
+  { id: 21, name: "Atila", numbers: [7, 13, 17, 31, 45, 57] },
+  { id: 22, name: "Atila", numbers: [19, 24, 33, 36, 38, 44] },
+  { id: 23, name: "Atila", numbers: [1, 2, 9, 15, 40, 60] },
+  { id: 24, name: "Atila", numbers: [2, 4, 19, 50, 55, 60] },
+  { id: 25, name: "Atila", numbers: [7, 9, 10, 20, 25, 54] },
+  { id: 26, name: "Atila", numbers: [2, 4, 10, 19, 54, 55] },
+  { id: 27, name: "Atila", numbers: [2, 4, 8, 9, 10, 12] },
+  { id: 28, name: "Atila", numbers: [13, 14, 16, 17, 18, 19] },
+  { id: 29, name: "Atila", numbers: [5, 9, 11, 12, 15, 17] },
+  { id: 30, name: "Atila", numbers: [7, 9, 10, 54, 55, 56] },
+  { id: 31, name: "Atila", numbers: [1, 40, 49, 54, 55, 56] },
+  { id: 32, name: "Atila", numbers: [5, 10, 23, 33, 34, 37] },
+  { id: 33, name: "Atila", numbers: [5, 10, 23, 33, 34, 53] },
+  { id: 34, name: "Atila", numbers: [7, 9, 10, 37, 53, 54] },
+  { id: 35, name: "Atila", numbers: [7, 9, 11, 26, 53, 55] },
+  { id: 36, name: "Atila", numbers: [23, 29, 35, 37, 53, 55] },
+  { id: 37, name: "Atila", numbers: [10, 12, 24, 33, 53, 54] },
+  { id: 38, name: "Atila", numbers: [7, 15, 23, 24, 50, 55] },
+  { id: 39, name: "Atila", numbers: [19, 23, 33, 54, 55, 57] },
+  { id: 40, name: "Atila", numbers: [10, 12, 19, 23, 33, 54] },
+
+  // ================= RODRIGO =================
+  { id: 41, name: "Rodrigo", numbers: [1, 6, 17, 22, 24, 35] },
+  { id: 42, name: "Rodrigo", numbers: [5, 11, 16, 22, 40, 53] },
+  { id: 43, name: "Rodrigo", numbers: [12, 27, 33, 41, 48, 59] },
+  { id: 44, name: "Rodrigo", numbers: [7, 8, 40, 44, 55, 60] },
+  { id: 45, name: "Rodrigo", numbers: [3, 15, 18, 34, 37, 52] },
+  { id: 46, name: "Rodrigo", numbers: [5, 11, 14, 23, 41, 56] },
+  { id: 47, name: "Rodrigo", numbers: [1, 13, 16, 33, 40, 53] },
+  { id: 48, name: "Rodrigo", numbers: [21, 24, 26, 45, 56, 58] },
+  { id: 49, name: "Rodrigo", numbers: [10, 13, 34, 44, 51, 55] },
+  { id: 50, name: "Rodrigo", numbers: [5, 13, 19, 20, 22, 33] },
+  { id: 51, name: "Rodrigo", numbers: [1, 5, 10, 15, 20, 25] },
+  { id: 52, name: "Rodrigo", numbers: [12, 13, 18, 26, 30, 60] },
+  { id: 53, name: "Rodrigo", numbers: [21, 36, 46, 49, 53, 57] },
+  { id: 54, name: "Rodrigo", numbers: [4, 12, 18, 19, 21, 32] },
+  { id: 55, name: "Rodrigo", numbers: [2, 3, 4, 5, 8, 11] },
+  { id: 56, name: "Rodrigo", numbers: [16, 23, 25, 42, 46, 52] },
+  { id: 57, name: "Rodrigo", numbers: [33, 37, 38, 44, 51, 55] },
+  { id: 58, name: "Rodrigo", numbers: [11, 14, 18, 24, 28, 31] },
+  { id: 59, name: "Rodrigo", numbers: [2, 9, 21, 31, 44, 60] },
+  { id: 60, name: "Rodrigo", numbers: [10, 13, 30, 41, 51, 57] },
+
+  // ================= FABIO =================
+  { id: 61, name: "Fabio", numbers: [7, 21, 30, 31, 38, 47] },
+  { id: 62, name: "Fabio", numbers: [1, 7, 21, 32, 45, 51] },
+  { id: 63, name: "Fabio", numbers: [7, 11, 34, 38, 40, 54] },
+  { id: 64, name: "Fabio", numbers: [4, 16, 20, 25, 51, 56] },
+  { id: 65, name: "Fabio", numbers: [3, 11, 22, 48, 49, 59] },
+  { id: 66, name: "Fabio", numbers: [14, 23, 28, 30, 38, 53] },
+  { id: 67, name: "Fabio", numbers: [5, 9, 13, 20, 22, 27] },
+  { id: 68, name: "Fabio", numbers: [3, 4, 5, 13, 27, 36] },
+  { id: 69, name: "Fabio", numbers: [9, 12, 35, 36, 37, 44] },
+  { id: 70, name: "Fabio", numbers: [6, 7, 42, 43, 48, 55] },
+  { id: 71, name: "Fabio", numbers: [8, 11, 12, 16, 44, 58] },
+  { id: 72, name: "Fabio", numbers: [2, 5, 25, 26, 32, 49] },
+  { id: 73, name: "Fabio", numbers: [23, 24, 33, 36, 38, 44] },
+  { id: 74, name: "Fabio", numbers: [1, 7, 21, 47, 48, 55] },
+  { id: 75, name: "Fabio", numbers: [14, 28, 38, 48, 50, 58] },
+  { id: 76, name: "Fabio", numbers: [35, 37, 39, 45, 48, 60] },
+  { id: 77, name: "Fabio", numbers: [8, 9, 11, 16, 42, 51] },
+  { id: 78, name: "Fabio", numbers: [3, 10, 11, 33, 52, 56] },
+  { id: 79, name: "Fabio", numbers: [18, 19, 37, 47, 48, 59] },
+  { id: 80, name: "Fabio", numbers: [14, 21, 38, 54, 57, 58] },
+
+  // ================= DAVID =================
+  { id: 81, name: "David", numbers: [5, 14, 23, 37, 48, 53] },
+  { id: 82, name: "David", numbers: [2, 10, 25, 33, 41, 54] },
+  { id: 83, name: "David", numbers: [4, 9, 22, 37, 43, 51] },
+  { id: 84, name: "David", numbers: [3, 5, 18, 32, 44, 53] },
+  { id: 85, name: "David", numbers: [7, 14, 23, 36, 48, 59] },
+  { id: 86, name: "David", numbers: [1, 10, 26, 33, 40, 51] },
+  { id: 87, name: "David", numbers: [6, 11, 22, 35, 37, 56] },
+  { id: 88, name: "David", numbers: [5, 12, 28, 30, 41, 57] },
+  { id: 89, name: "David", numbers: [8, 19, 23, 34, 47, 54] },
+  { id: 90, name: "David", numbers: [2, 13, 24, 39, 46, 53] },
+  { id: 91, name: "David", numbers: [4, 17, 29, 36, 45, 51] },
+  { id: 92, name: "David", numbers: [3, 18, 25, 32, 49, 60] },
+  { id: 93, name: "David", numbers: [6, 15, 20, 37, 43, 52] },
+  { id: 94, name: "David", numbers: [1, 14, 23, 38, 44, 59] },
+  { id: 95, name: "David", numbers: [5, 11, 27, 30, 48, 55] },
+  { id: 96, name: "David", numbers: [8, 16, 21, 34, 42, 57] },
+  { id: 97, name: "David", numbers: [9, 12, 28, 37, 41, 53] },
+  { id: 98, name: "David", numbers: [7, 10, 24, 35, 46, 58] },
+  { id: 99, name: "David", numbers: [2, 5, 26, 33, 47, 51] },
+  { id: 100, name: "David", numbers: [3, 16, 22, 36, 48, 54] },
+];
+
 const jogosFriends = [];
 
 let jogosAtuais = [];
@@ -126,6 +238,7 @@ function verificarSenha() {
   const listHiae = document.getElementById("listHiae");
   const listFamily = document.getElementById("listFamily");
   const listFriends = document.getElementById("listFriends");
+  const listNewset = document.getElementById("listNewset");
 
   document.getElementById("resultBody").innerHTML = "";
   document.getElementById("resultTable").classList.add("hidden");
@@ -136,18 +249,21 @@ function verificarSenha() {
     listHiae.classList.add("hidden");
     listFriends.classList.add("hidden");
     listFamily.classList.remove("hidden");
+    listNewset.classList.add("hidden");
   } else if (senha === "core") {
     jogosAtuais = jogosEinstein;
     errorMessage.classList.add("hidden");
     listHiae.classList.remove("hidden");
     listFriends.classList.add("hidden");
     listFamily.classList.add("hidden");
-  } else if (senha === "friends") {
-    jogosAtuais = jogosFriends;
+    listNewset.classList.add("hidden");
+  } else if (senha === "newset") {
+    jogosAtuais = jogosNewSet;
     errorMessage.classList.add("hidden");
     listHiae.classList.add("hidden");
-    listFriends.classList.remove("hidden");
+    listFriends.classList.add("hidden");
     listFamily.classList.add("hidden");
+    listNewset.classList.remove("hidden");
   } else {
     errorMessage.classList.remove("hidden");
     return;
